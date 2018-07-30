b57c3a27507bcafcf53dd7131d8fb34e26d0a350
jibyeol <cjfrtt1234@naver.com>
2018-07-30 13:32:04 +0900

    설정

---
+++ lobdingmachine/src/test/java/kr/co/ldcc/lobdingmachine/lobdingmachine/LobdingmachineApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.co.ldcc.lobdingmachine.lobdingmachine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LobdingmachineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
