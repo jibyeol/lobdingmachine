74c06a97b82629d2dccf73f2d49d4990e8fd0182
jibyeol <cjfrtt1234@naver.com>
2018-07-30 14:15:10 +0900

    test

--- lobdingmachine/src/main/java/kr/co/ldcc/lobdingmachine/controller/TestController.java
+++ lobdingmachine/src/main/java/kr/co/ldcc/lobdingmachine/controller/TestController.java
@@ -1,5 +1,8 @@
 package kr.co.ldcc.lobdingmachine.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +12,16 @@ public class TestController extends BaseController {
     @RequestMapping("/test")
     public String allTest(){
         return "test";
+    }
+    
+    @RequestMapping("/test2")
+    public Map<String, String> test2(){
+    	Map<String, String> result = new HashMap<String, String>();
+    	result.put("key1", "value1");
+    	result.put("key2", "value2");
+    	result.put("key3", "value3");
+    	result.put("key4", "value4");
+    	return result;
     }
 
 }
