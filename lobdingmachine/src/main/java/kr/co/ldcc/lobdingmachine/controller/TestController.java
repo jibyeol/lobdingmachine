75b3a4f26299e800be824113d50d84552604d232
jibyeol <cjfrtt1234@naver.com>
2018-07-30 14:04:52 +0900

    test controller

--- lobdingmachine/src/main/java/kr/co/ldcc/lobdingmachine/controller/TestController.java
+++ lobdingmachine/src/main/java/kr/co/ldcc/lobdingmachine/controller/TestController.java
@@ -1,32 +1,14 @@
 package kr.co.ldcc.lobdingmachine.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-//@RestController
-@RequestMapping("/test")
+@RestController
 public class TestController extends BaseController {
 
-    @RequestMapping("/all")
+    @RequestMapping("/test")
     public String allTest(){
-        return "test/all";
-    }
-
-    @RequestMapping("/general")
-    public String generalTest(){
-        return "test/general";
-    }
-
-    @RequestMapping("/admin")
-    public String adminTest(){
-        return "test/admin";
-    }
-    
-    @RequestMapping("/chart")
-    public String chartTest(){
-    	return "test/chart";
+        return "test";
     }
 
 }
