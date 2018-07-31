40fc0c5085f99f73a0b3b6e59f9c0fc58e295839
jibyeol <cjfrtt1234@naver.com>
2018-07-31 14:29:03 +0900

    관리자 디자인 추가

---
+++ lobdingmachine/src/main/java/kr/co/ldcc/lobdingmachine/controller/admin/ProductController.java
@@ -0,0 +1,16 @@
+/*package kr.co.ldcc.lobdingmachine.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+
+	@RequestMapping("/product")
+	public String productPage() {
+		return "admin/product/main";
+	}
+	
+	
+}
+*/
