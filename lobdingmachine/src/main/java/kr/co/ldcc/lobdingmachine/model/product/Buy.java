1bdea8aaaf2587ffb741ba15292236101058f9c4
jibyeol <cjfrtt1234@naver.com>
2018-07-31 14:24:27 +0900

    ddsdfs

---
+++ lobdingmachine/src/main/java/kr/co/ldcc/lobdingmachine/model/product/Buy.java
@@ -0,0 +1,30 @@
+package kr.co.ldcc.lobdingmachine.model.product;
+
+import java.util.List;
+
+public class Buy {
+
+	List<Product> productList;
+	String phoneNumber;
+	String cardNumber;
+	
+	public List<Product> getProductList() {
+		return productList;
+	}
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	
+}
