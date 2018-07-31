f7fad50e827111ac34cb1b78774acb360f373a93
LOTTE <LOTTE@LOTTE-PC12>
2018-07-31 15:29:42 +0900

    ㅠㅠㅠㅠㅠ

--- lobdingmachine/src/main/java/kr/co/ldcc/lobdingmachine/dao/ProductDao.java
+++ lobdingmachine/src/main/java/kr/co/ldcc/lobdingmachine/dao/ProductDao.java
@@ -1,14 +1,17 @@
 package kr.co.ldcc.lobdingmachine.dao;
 
 import java.util.List;
+import java.util.Scanner;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.co.ldcc.lobdingmachine.model.product.Buy;
 import kr.co.ldcc.lobdingmachine.model.product.Product;
 
 @Mapper
 public interface ProductDao {
 
 	List<Product> getProductByCategoryIdx(int categoryIdx);
-	
+	//todo: 로그인했을 때 id 받아오는것
+	List<Product> setbuyProduct(Buy buy);
 }
